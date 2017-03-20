29206b3b3eb25fabaa051c161435d4cc8aa6d44c
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-03-20 15:38:01 +0800

    added comment in arduino code

--- arduino/ldr/ldr.ino
+++ arduino/ldr/ldr.ino
@@ -1,3 +1,15 @@
+// mini iOT projects
+// created by superoo7 19/3/2017
+
+// from Light dependant resistor take analog value to put in database
+// mainly for iot purpose
+
+// In arduino
+// Connect LDR pin with 10k ohms parallel to A0 pin
+// to create a potential difference for the ldr to sense value
+// in Arduino analog pin
+
+
 int LDR_Pin = A0; //analog pin 0
 
 void setup() {
