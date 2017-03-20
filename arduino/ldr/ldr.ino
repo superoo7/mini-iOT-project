f9c2012e19381d270c066aade9afc44c835007e7
Johnson Lai MBP <jlwhoo7@gmail.com>
2017-03-20 15:31:27 +0800

    done arduino part programming

---
+++ arduino/ldr/ldr.ino
@@ -0,0 +1,13 @@
+int LDR_Pin = A0; //analog pin 0
+
+void setup() {
+   Serial.begin(9600); //Baud rate: 9600
+
+}
+
+void loop() {
+  int LDRReading = analogRead(LDR_Pin); 
+
+  Serial.println(LDRReading);
+  delay(250); //just here to slow down the output for easier reading
+}
